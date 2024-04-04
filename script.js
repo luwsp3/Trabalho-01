8a5cb107493c8ce47cccc0e95c6849da985efdee
luwsp3 <166078742+luwsp3@users.noreply.github.com>
2024-04-04 20:20:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function redirectMetodos() {
+    window.location.href = "metodos.html"
+}
+
+function pomodoroBttn() {
+    window.location.href = "https://brasilescola.uol.com.br/dicas-de-estudo/tecnica-pomodoro-que-e-e-como-funciona.htm"
+}
+
+function fichBttn() {
+    window.location.href = "https://www.todamateria.com.br/fichamento/"
+}
+
+function autoexpBttn() {
+    window.location.href = "https://oficinadamente.com/auto-explicacao-quer-aprender-explique-voce-mesmo/"
+}
+
+function interBttn() {
+    window.location.href = "https://planejativo.com/tecnica-de-estudo-intercalado/"
+}
